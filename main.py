9ae30abdccf28c74e65c60f73099889992fbf445
Abhishek <abhishek620380@gmail.com>
2023-11-28 06:21:06 +0530

    TC

---
+++ main.py
@@ -0,0 +1,89 @@
+from selenium import webdriver
+import os 
+from select import select
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC3
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import pandas as pd
+import os
+from idna import valid_contextj
+from datetime import datetime, timedelta
+import logging
+import shutil
+
+op = webdriver.ChromeOptions()
+# op.add_argument('--headless=new')
+prefs = {
+    'profile.default_content_settings.popups': 0,
+    'download.default_directory' : r"/home/administrator/cbs_bag_hold/data",
+    'directory_upgrade': True
+}
+op.add_experimental_option('prefs' , prefs)
+driver = webdriver.Chrome(options=op)
+
+
+driver.get("http://10.24.1.71/tc")
+
+print(driver.title)
+
+def login():
+    username = driver.find_element(By.XPATH , "/html/body/div[2]/div[2]/div/div/form/div/div[4]/input[1]")
+    username.send_keys("ca.2670054")
+
+    password = driver.find_element(By.XPATH , "/html/body/div[2]/div[2]/div/div/form/div/div[4]/input[2]")
+    password.send_keys("Veer@809")
+
+
+    try:
+        cross = driver.find_element(By.XPATH , "/html/body/div[4]/div/button")
+        cross.click()
+    except:
+        print("Cross Button Failed")
+
+    time.sleep(1)
+
+    submit = driver.find_element(By.XPATH , "/html/body/div[2]/div[2]/div/div/form/div/div[4]/div[4]/button/span")
+    submit.click()
+    time.sleep(5)
+
+def ekart_Selection():
+    try:
+        ekart = driver.find_element(By.XPATH , "/html/body/form/label[2]")
+        ekart.click()
+    except Exception as E:
+        print(f"Ekart Not Found {E}")
+    try:
+        submit = driver.find_element(By.XPATH , "/html/body/form/div/button")
+        submit.submit()
+
+    except Exception as e:
+        print(f"Submit Button not Found  {e}")
+
+
+def facility_selection():
+    try:
+        driver.get("http://10.24.1.71/tc?location=174222&locationExternalId=171385&locationName=MotherHub_YKB")
+    except Exception as E:
+        print(f"Error {E}")
+
+def vehicle_selector(vehicle_number , type):
+    try:
+        driver.get(f"http://10.24.1.71/tc/loading?contract_type={type}&location=174222&locationExternalId=171385&locationName=MotherHub_YKB&ptID=1&vehicle={vehicle_number}")
+    except Exception as E:
+        print(E)
+
+
+
+ekart_Selection()
+time.sleep(1)
+login()
+time.sleep(2)
+vehicle_selector("HR38AAA3077" , "FLEET")
+time.sleep(50000)
